e10275784410dfdefbfd5c8296614c5f384a5071
iPan7 <ipanlilio7@gmail.com>
2021-11-27 16:47:52 -0800

    added crowsale contract and unit tests for token contract, plus chai dependencies

--- test/MyToken.test.js
+++ test/MyToken.test.js
@@ -12,11 +12,32 @@ chai.use(chaiAsPromised);
 const expect = chai.expect;
 
 contract("Token Test", async (accounts) => {
-    it("all tokens should be in my account", async () => {
+
+    const [deployerAccount, recipient, anotherAccount] = accounts;
+
+    it("all tokens are in my account", async () => {
         let instance = await Token.deployed();
         let totalSupply = await instance.totalSupply();
-        // let balance = await instance.balanceOf(accounts[0]);
-        // assert.equal(balance.valueOf(), initialSupply.valueOf(), "The balance was not the same");
         expect(await instance.balanceOf(accounts[0])).to.be.a.bignumber.equal(totalSupply);
-    })
+    });
+
+    it("it is not possible to send more tokens than available in total", async () => {
+        let instance = await Token.deployed();
+        let balanceOfDeployer = await instance.balanceOf(deployerAccount);
+
+        expect(instance.transfer(recipient, new BN(balanceOfDeployer+2))).to.eventually.be.rejected;
+        expect(instance.balanceOf(deployerAccount)).to.eventually.be.a.bignumber.equal(balanceOfDeployer);
+    });
+
+    it("it is possible to send tokens between accounts", async() => {
+        const sendTokens = 1;
+        let instance = await Token.deployed();
+        let totalSupply = await instance.totalSupply();
+        expect(instance.balanceOf(deployerAccount)).to.eventually.be.a.bignumber.equal(totalSupply);
+        expect(instance.transfer(recipient, sendTokens)).to.eventually.be.fulfilled;
+        expect(instance.balanceOf(deployerAccount)).to.eventually.be.a.bignumber.equal(totalSupply.sub(new BN(sendTokens)));
+        expect(instance.balanceOf(recipient)).to.eventually.be.a.bignumber.equal(new BN(sendTokens));
+    });
+
+    // it is possible to 
 })
