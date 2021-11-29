31431dac8c25078bac10a6b48c47cbaaee86fbe8
iPan7 <ipanlilio7@gmail.com>
2021-11-28 18:59:52 -0800

    Updates

---
+++ precache-manifest.721c7182beb8eb3244b3d86c437d0b03.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d128bd20fbaeca1ee272a84cef8b32a",
+    "url": "/blockchain-developer-bootcamp-final-project/index.html"
+  },
+  {
+    "revision": "a09cc1f73d2d39e1112f",
+    "url": "/blockchain-developer-bootcamp-final-project/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "eb7abc1e96b6d847f74e",
+    "url": "/blockchain-developer-bootcamp-final-project/static/js/2.d87ce983.chunk.js"
+  },
+  {
+    "revision": "a09cc1f73d2d39e1112f",
+    "url": "/blockchain-developer-bootcamp-final-project/static/js/main.e13779e6.chunk.js"
+  },
+  {
+    "revision": "974fe9a407c487a617cb",
+    "url": "/blockchain-developer-bootcamp-final-project/static/js/runtime-main.dca84915.js"
+  }
+]);
